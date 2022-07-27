44984a680a66e16e0c2c31d42246b84e54bc9097
mjha2475 <73065205+mjha2475@users.noreply.github.com>
2022-07-27 15:53:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState,useEffect} from 'react';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import products from'./products.json';
+// import CardItem from './components/CardItem';
 function App() {
+  const[money,setMoney]=useState(3000000000000000000000);
+  const[total,setTotal]=useState(0);
+  const[basket,setBasket]=useState([]);
+
+  
+  useEffect(() => {
+    setTotal(
+      basket.reduce((acc, item) => {
+        return (
+          acc +
+          item.amount * products.find((product) => product.id === item.id).price
+        );
+      }, 0)
+    );
+  }, [basket]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Header money={money} total={total}/>
+       <div className='container'>
+         <Card basket={basket} setBasket={setBasket} total={total} money={money} products={products}/>
+       </div>
+       {basket.length>0 && <Footer total={total}  basket={basket} setBasket={setBasket}/>}
+      
     </div>
   );
 }
+
 
 export default App;
